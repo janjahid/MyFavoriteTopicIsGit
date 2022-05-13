fc48bb9b3c0c7f7b83c54b92d11a3fea9ad31819
janmohammd jahed <janmohammad.jahid@tekschool.us>
2022-05-12 21:42:05 -0500

    this is new

---
+++ src/us/tekschool/git/geture/LabGit.java
@@ -0,0 +1,14 @@
+package us.tekschool.git.geture;
+
+public class LabGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int i=10;
+ int c=12;
+		 int r = i+c;
+ System.out.println(r);
+ 
+	}
+
+}
